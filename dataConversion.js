2623e0c59e66918a8929b2627e0b2a4b587e2934
Valorie Portem <valorieportem@gmail.com>
2024-09-15 09:22:45 +0800

    initial commit

---
+++ dataConversion.js
@@ -0,0 +1,20 @@
+let price = "200";
+
+let v = parseInt(price);
+console.log("Using parseInt():", v);
+let A = Number(price);
+console.log("Using Number():", A);
+let l = +price;
+console.log("Using + operator:", l);
+
+
+let distance = 150;
+let o = distance.toString();
+console.log("Using toString():", o);
+let r = String(distance);
+console.log("Using String():", r);
+
+
+let weight = "72.5";
+let i = parseFloat(weight);
+console.log("Using parseFloat():", i);
